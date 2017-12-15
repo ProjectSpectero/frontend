0f8bbcc36fdb6f2bc19072d4a922ef14359b2ad6
Anatolie Diordita <contact@anatolie.me>
2017-12-15 15:20:04 -0500

    DFRON-59: Redirect /login to /dashboard if user already logged in.

--- src/router/index.js
+++ src/router/index.js
@@ -12,16 +12,23 @@ import NotFound from '@/components/NotFound'
 Vue.use(Router)
 Vue.use(Meta)
 
+/**
+ * Meta keys for router:
+ * 
+ * auth:     redirect user when not logged in (login required from user to access route)
+ * antiAuth: redirect user when already authenticated (route for non-logged in users only)
+ */
 const router = new Router({
   mode: 'history',
   routes: [
-    { path: '/login', name: 'login', component: Login },
+    { path: '/login', name: 'login', component: Login, meta: { antiAuth: true } },
     { path: '/users', name: 'users', component: Users, meta: { auth: true } },
     { path: '*', name: 'notFound', component: NotFound }
   ]
 })
 
 router.beforeEach((to, from, next) => {
+  
   // Handle routes requiring authentication
   if (to.matched.some(record => record.meta.auth)) {
     // Get decoded JWT token (if any), check user authentication against API
@@ -30,9 +37,22 @@ router.beforeEach((to, from, next) => {
     } else {
       next({ name: 'login', query: { redirect: to.fullPath } })
     }
-  } else {
+  }
+
+  // Handle routes that arent view-able by already logged in users (default to /dashboard)
+  else if (to.matched.some(record => record.meta.antiAuth)) {
+    if (auth.parseJWT().error === null) {
+      next({ name: 'dashboard' })
+    } else {
+      next()
+    }
+  }
+  
+  // Handle regular routes
+  else {
     next()
   }
+
 })
 
 export default router
