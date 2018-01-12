78ef4abc57d6974baefe145165236fd33db418e4
Sergio Castro <sergio.castro.vale@gmail.com>
2018-01-12 17:59:44 +0000

    DFRON-117: Updated dev env configs to use our static environment again

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
+  DAEMON_ENDPOINT: '"23.172.128.100"',
+  DAEMON_PORT: '""',
   DAEMON_VERSION: '"1"'
 })
